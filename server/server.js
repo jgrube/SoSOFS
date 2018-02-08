d3d266ba1dd61741d43367cf425cb83e17cb65b4
jgrube <kb1kyh@gmail.com>
2018-02-08 08:53:19 -0500

    Added received data processing to server.

--- server/server.js
+++ server/server.js
@@ -3,17 +3,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const net = require("net");
+const fs = require("fs-extra");
 let logger = require("bristol");
 const config = require("../config.json");
 let server = net.createServer();
 server.on("connection", connectionHandler);
+function parseRxData(data) {
+    logger.trace(`ParseRX: ${data}`);
+    fs.outputFile("./data/test_file.txt", data);
+}
 function connectionHandler(connection) {
     logger.trace(`Client connection from ${connection.remoteAddress}:${connection.remotePort}`);
+    let incomingData = Buffer.alloc(0);
     connection.on("data", (data) => {
         logger.trace(`Client (${connection.remoteAddress}:${connection.remotePort}) data: ${data}`);
+        incomingData = Buffer.concat([incomingData, data]);
     })
         .on("close", (error) => {
         logger.trace(`Client (${connection.remoteAddress}:${connection.remotePort}) connection closed`);
+        setImmediate(parseRxData, incomingData);
     })
         .on("error", (error) => {
         logger.warn(`Client (${connection.remoteAddress}:${connection.remotePort}) error: ${error}`);
--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@
 "use strict";
 
 import net = require("net");
+import fs = require("fs-extra");
 
 let logger = require("bristol");
 const config: Config = require("../config.json");
@@ -10,14 +11,23 @@ const config: Config = require("../config.json");
 let server = net.createServer();
 server.on("connection", connectionHandler);
 
+function parseRxData(data: Buffer) {
+    logger.trace(`ParseRX: ${data}`);
+    fs.outputFile("./data/test_file.txt", data);
+}
+
 function connectionHandler(connection: net.Socket): void {
     logger.trace(`Client connection from ${connection.remoteAddress}:${connection.remotePort}`);
 
+    let incomingData: Buffer = Buffer.alloc(0);
+
     connection.on("data", (data: Buffer) => {
         logger.trace(`Client (${connection.remoteAddress}:${connection.remotePort}) data: ${data}`);
+        incomingData = Buffer.concat([incomingData, data]);
     })
     .on("close", (error: boolean) => {
         logger.trace(`Client (${connection.remoteAddress}:${connection.remotePort}) connection closed`);
+        setImmediate(parseRxData, incomingData);
     })
     .on("error", (error: NodeJS.ErrnoException) => {
         logger.warn(`Client (${connection.remoteAddress}:${connection.remotePort}) error: ${error}`);
